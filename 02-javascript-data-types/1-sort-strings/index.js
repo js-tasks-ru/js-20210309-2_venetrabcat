752cd8519ac7acbee32074c5158aecbd3ad3f16f
ilabartenev <ilia.bartenev@novardis.com>
2021-03-15 20:07:57 +0300

    create sort string function

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,15 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
+  const sortArr = [...arr];
+  const locales = ['ru', 'en'];
+  const options = {
+    caseFirst: 'upper',
+  };
 
+  sortArr.sort((a, b) => {
+    return param === 'desc' ? b.localeCompare(a, locales, options) : a.localeCompare(b, locales, options);
+  });
+
+  return sortArr;
 }
