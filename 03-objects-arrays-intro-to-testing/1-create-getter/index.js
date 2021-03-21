54023efcd38c9c2efb5fa8622c64fc3ee7c432ff
ilabartenev <ilia.bartenev@novardis.com>
2021-03-21 17:19:02 +0300

    * function createGetter

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,20 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  const arrayPath = path.split('.');
 
+  const searchPath = (obj) => {
+    for (let value of Object.values(arrayPath)) {
+      if (obj[value]) {
+
+        if (typeof obj[value] === "object") {
+          return searchPath(obj[value]);
+        }
+
+        return obj[value];
+      }
+    }
+  };
+
+  return searchPath;
 }
