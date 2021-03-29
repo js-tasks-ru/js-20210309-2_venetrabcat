be60666ba46e24a87efd71d5f866eca2e01ba1d2
ilabartenev <ilia.bartenev@novardis.com>
2021-03-29 14:35:28 +0300

    create notification component

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,57 @@
 export default class NotificationMessage {
+  static notification = [];
 
+  constructor(message = '', {
+    duration = 2000,
+    type = 'success',
+  } = {}) {
+    this.message = message;
+    this.duration = duration;
+    this.type = type;
+    this.setElement();
+  }
+
+  setElement() {
+    const element = document.createElement('div');
+
+    element.innerHTML = this.template;
+    this.element = element.firstElementChild;
+  }
+
+  get template() {
+    return `<div class="notification ${this.type}" style="--value:${this.duration / 1000}s">
+              <div class="timer"></div>
+              <div class="inner-wrapper">
+                <div class="notification-header">${this.type}</div>
+                <div class="notification-body">${this.message}</div>
+              </div>
+            </div>`;
+  }
+
+  show(elem) {
+    this.remove();
+
+    if (elem !== undefined) {
+      elem.append(this.element);
+    } else {
+      document.body.append(this.element);
+    }
+
+    this.timeOut();
+  }
+
+  remove() {
+    NotificationMessage.notification.forEach(el => el.remove());
+    NotificationMessage.notification.push(this.element);
+  }
+
+  timeOut() {
+    setTimeout(() => {
+      this.destroy();
+    }, this.duration);
+  }
+
+  destroy() {
+    this.element.remove();
+  }
 }
